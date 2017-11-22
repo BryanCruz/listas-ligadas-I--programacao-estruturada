d18ddc5c245e480e6f0d209177945028124fdf79
BryanCruz <bryan.bialokur@gmail.com>
2017-11-22 18:59:28 -0200

    versão 2 do exercício

--- listas.c
+++ listas.c
@@ -1,58 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "listas.h"
 
-typedef struct s_no * no;
+// 1. (EXERCÍCIO) Cria um novo nó da lista.
+//    Mudar a implementação da função novo
+//    para alocar muitos nós de uma vez e usar a lista
+//    livres para guardar os blocos que ainda não estão
+//    sendo usados. Como visto em sala...
+no novo(int item);
 
-struct s_no {
-  int item;
-  no  prox;
-};
-
-// 1. Cria um novo nó da lista.
-no novo(int item) {
-  no x = (no) malloc(sizeof(struct s_no));
-
-  if (x == NULL) {
-    fprintf(stderr, "Memória insuficiente.\n");
-    exit(1);
-  }
-
-  x->item = item;
-  x->prox = NULL;
-
-  return x;
-}
-
-// 2. Deleta um nó.
-void deleta(no x) {
-  free(x);
-}
+// 2. (EXERCÍCIO) Deleta um nó.
+//    Adaptar a função deleta para trabalhar
+//    com a lista de nós livres. Como visto em sala...
+void deleta(no x);
 
 // 3. Insere um nó no início da lista (como PUSH).
-//    (Supõem que x e inicio são ambos diferentes de NULL.)
-//    (Mas *inicio pode ser NULL.)
-void insere_inicio(no *inicio, no x) {
-  x->prox = *inicio;
-  *inicio = x;
+//    (Supõem que x e ini são ambos diferentes de NULL.)
+//    (Mas *ini pode ser NULL.)
+void insere_inicio(no *ini, no x) {
+  x->prox = *ini;
+  *ini = x;
 }
 
 // 4. Remove o primeiro nó da lista.
 //    (Se o valor de retorno fosse o item do nó removido,
 //    essa função seria como POP.)
-void remove_inicio(no *inicio) {
-  no x = *inicio;
-  if (*inicio != NULL) {
-    *inicio = x->prox;
+void remove_inicio(no *ini) {
+  no x = *ini;
+  if (*ini != NULL) {
+    *ini = x->prox;
     deleta(x);
   }
 }
 
 // 5. Imprime todos os elementos de uma lista
 //    (supondo que sejam inteiros).
-void imprime(no inicio) {
-  no x;
-  for (no x = inicio; x != NULL; x = x->prox)
-    printf("%d ", x->item);
+void imprime(no x) {
+  for (; x != NULL; x = x->prox)
+    printf("%d", x->item);
   printf("\n");
 }
 
@@ -74,87 +59,62 @@ no buscaR(no inicio, int item) {
   return buscaR(inicio->prox, item);
 }
 
-// 8 (EXERCÍCIO) Devolve o último nó de uma lista.
-no final(no inicio){
-  if(inicio == NULL){
+// 8. Devolve o último nó de uma lista.
+no final(no x) {
+  if (x == NULL)
     return NULL;
+
+  while (x->prox != NULL)
+    x = x->prox;
+
+  return x;
+};
+
+// 9. Insere nó no final.
+//    (Supõem que x e ini são ambos diferentes de NULL.)
+//    (Mas *ini pode ser NULL.)
+void insere_final(no *ini, no x) {
+  if (*ini == NULL) {
+    *ini = x;
+    return;
   }
 
-  while(inicio->prox != NULL){
-    inicio = inicio->prox;
-  }
-  return inicio;
+  no y = final(*ini);
+  y->prox = x;
 }
 
-// 9. (EXERCÍCIO) Insere nó no final.
-//    (Supõem que x e inicio são ambos diferentes de NULL.)
-//    (Mas *inicio pode ser NULL.)
-void insere_final(no *inicio, no x){
-  if(*inicio == NULL){
-    *inicio = x;
-  }else{
-    no y = final(*inicio);
-    y->prox = x;
-  }
-}
-
-// 10. (EXERCÍCIO) Insere nó no final, recursivamente.
-//    (Supõem que x e inicio são ambos diferentes de NULL.)
-//    (Mas *inicio pode ser NULL.)
-void insere_finalR(no *inicio, no x){
-  if(*inicio == NULL){
-    *inicio = x;
-  }else{
-    insere_finalR(&((*inicio)->prox), x);
-  }
+// 10. Insere nó no final, recursivamente.
+//    (Supõem que x e ini são ambos diferentes de NULL.)
+//    (Mas *ini pode ser NULL.)
+void insere_finalR(no *ini, no x) {
+  if (*ini == NULL)
+    *ini = x;
+  else
+    insere_finalR(&((*ini)->prox), x);
 }
 
 // 11. Remove primeiro nó que contém item.
-//    (Supõem que inicio é diferente de NULL.
-//    (*inicio pode ser NULL.)
+//    (Supõem que ini é diferente de NULL.
+//    (*ini pode ser NULL.)
 //    (EXERCÍCIO: entender o que a função faz.)
-void remove_um(no *inicio, int item) {
-  no x = *inicio, *aux = inicio;
-  while(x != NULL && x->item != item){
-    aux = &(x->prox);
-    x = x->prox;
-  }
+void remove_um(no *ini, int item) {
+  if (*ini == NULL)
+    return;
 
-  if(x != NULL){
-    *aux = x->prox;
+  no x, *prev = ini;
+  for (x = (*ini); x != NULL && x->item != item;
+       prev = &(x->prox), x = x->prox);
+
+  if (x != NULL) {
+    *prev = x->prox;
     deleta(x);
   }
-}
-
-// 11. Remove primeiro nó que contém item.
-//     (Provavelmente a 1ª idéia que vem à mente.)
-void remove_um_v1(no *inicio, int item) {
-  if (*inicio == NULL)
-    return;
-
-  no x = *inicio, y;
-
-  if (x->item == item) {
-    *inicio = x->prox;
-    deleta(x);
-    return;
-  }
-
-  while (x->prox != NULL && x->prox->item != item)
-    x = x->prox;
-
-  if (x->prox == NULL)
-    return;
-
-  y = x->prox;
-  x->prox = y->prox;
-  deleta(y);
 }
 
 // 12. (EXERCÍCIO) Remove todos os nós contendo item.
-void remove_todos(no *inicio, int item){
+void remove_todos(no *ini, int item){
   //x é copia, *aux é referencia, aux é cópia da referencia
-  no x = *inicio, *aux = inicio;
+  no x = *ini, *aux = ini;
 
   while(x != NULL){
     if(x->item == item){
@@ -171,69 +131,52 @@ void remove_todos(no *inicio, int item){
 
 // 13. (EXERCÍCIO) Remove todos os nós contendo item, recursivo.
 //     Este fica mais simples que o anterior.
-void remove_todosR(no *inicio, int item){
-  if(*inicio != NULL){
-    no x = *inicio;
+void remove_todosR(no *ini, int item){
+  if(*ini != NULL){
+    no x = *ini;
     remove_todosR(&(x->prox), item);
 
     if(x->item == item){
-       *inicio = x->prox;
+       *ini = x->prox;
        deleta(x);
     }
   }
 }
 
-// 14. (EXERCÍCIO) Cria uma cópia da lista dada
+// 14. Cria uma cópia da lista dada
 //     (copiar em outras posições de memória, é claro).
-no copia(no inicio){
-  no inicioCopia = NULL;
+no copia(no inicio) {
+  if (inicio == NULL)
+    return NULL;
 
-  if(inicio != NULL){
-    inicioCopia = novo(inicio->item);
-    inicio = inicio->prox;
-
-    while(inicio != NULL){
-      insere_final(&inicioCopia, novo(inicio->item));
-      inicio = inicio->prox;
-    }
-  }
-  return inicioCopia;
+  no x = novo(inicio->item);
+  x->prox = copia(inicio->prox);
+  return x;
 }
 
 // 15. Inverte a lista.
-void inverte(no *inicio) {
-  no inv = NULL, x = *inicio;
+void inverte(no *ini) {
+  no x = *ini;
+  *ini = NULL;
 
-  while (*inicio != NULL) {
-    x = *inicio;
-    *inicio = (*inicio)->prox;
-    x->prox = inv;
-    inv = x;
+  while (x != NULL) {
+    no y = x->prox;
+    x->prox = *ini;
+    *ini = x;
+    x = y;
   }
-
-  *inicio = x;
 }
 
-// 16. Função recursiva para inverter uma lista.
-//     (EXERCÍCIO: entender o que a função faz.)
-void inverteR(no *head, no *tail) {
-  if (*head == *tail) return;
-  inverteR(&((*head)->prox), tail);
-  (*tail)->prox = *head;
-  *tail = *head;
-  *head = (*head)->prox;
-  (*tail)->prox = NULL;
-}
-
-// 17. (EXERCÍCIO) Função recursiva para inverter uma lista
+// 16. (EXERCÍCIO) Função recursiva para inverter uma lista
 //     Agora só com o ponteiro para (o ponteiro para) o
 //     primeiro nó sendo passado como parâmetro.
-void inverteR2(no *head);
+void inverteR2(no *ini);
 
 /****************************************************/
 /****************************************************/
 /****************************************************/
 
+/*
 void imprimir_teste(char * nome){
   printf("Testa %s\n", nome);
 }
@@ -478,3 +421,4 @@ int main() {
 
   return 0;
 }
+*/
